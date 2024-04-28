c24cc622571a7d1680c138ce294efb59352045ea
Sharaf <sharafa839@gmail.com>
2024-04-28 04:47:43 +0300

    - ApiError   - create error struct confirm on error

---
+++ ConcurrencyNetworkLayer/NetworkLayer/ApiError.swift
@@ -0,0 +1,44 @@
+//
+//  ApiError.swift
+//  ConcurrencyNetworkLayer
+//
+//  Created by Sharaf on 4/28/24.
+//
+
+import Foundation
+
+struct ApiError: Error {
+    
+    //MARK: -  Properties
+    
+    var statusCode: Int!
+    let errorCode: String
+    var message: String
+    
+    //MARK: - Init
+    
+    init(statusCode: Int! = 0, errorCode: String, message: String) {
+        self.statusCode = statusCode
+        self.errorCode = errorCode
+        self.message = message
+    }
+    
+    var erroCodeNumber: String {
+        let numberString: String = errorCode.components(separatedBy: CharacterSet.decimalDigits.inverted).joined()
+        return numberString
+    }
+    
+    private enum CodingKeys: String, CodingKey {
+        case errorCode
+        case message
+    }
+}
+
+extension ApiError: Decodable {
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.message = try container.decode(String.self, forKey: .message)
+        self.errorCode = try container.decode(String.self, forKey: .errorCode)
+    }
+}

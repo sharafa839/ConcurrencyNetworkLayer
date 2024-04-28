b1a5dbd0a16f6d29bdfbb0b86ed98589afdd7481
Sharaf <sharafa839@gmail.com>
2024-04-28 04:16:02 +0300

    Initial Commit

---
+++ ConcurrencyNetworkLayer/ConcurrencyNetworkLayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencyNetworkLayerApp.swift
+//  ConcurrencyNetworkLayer
+//
+//  Created by Sharaf on 4/28/24.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrencyNetworkLayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

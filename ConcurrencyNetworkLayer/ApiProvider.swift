847066e5c93a3060188207390223f8b5f80eae59
Sharaf <sharafa839@gmail.com>
2024-04-28 04:32:32 +0300

    - Api Provider   - create api provider protocol contains properties of url request components   - create api extension and  create function to return url request after declare

--- ConcurrencyNetworkLayer/ApiProvider.swift
+++ ConcurrencyNetworkLayer/ApiProvider.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+
+protocol ApiProvider {
+    var scheme: String { get }
+    var method: RequestMethod { get }
+    var baseURL: String { get }
+    var path: String { get }
+    var header: [String: String] { get }
+    var queryItems: [URLQueryItem]? { get }
+    var body: [String: Any]? { get }
+    var mockFile: String? { get }
+}
+
+extension ApiProvider {
+    
+    func asURLRequest() -> URLRequest {
+        var urlComponents =  URLComponents()
+        urlComponents.path = path
+        urlComponents.scheme = scheme
+        urlComponents.host = baseURL
+        if let queryItems = queryItems {
+            urlComponents.queryItems = queryItems
+        }
+        guard let url = urlComponents.url else { return }
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = method.rawValue
+        urlRequest.allHTTPHeaderFields = header
+        
+        if let body = body {
+            do {
+                urlRequest.httpBody = try JSONSerialization.data(withJSONObject: body, options: [])
+            } catch {
+                
+            }
+        }
+        return urlRequest
+    }
+}

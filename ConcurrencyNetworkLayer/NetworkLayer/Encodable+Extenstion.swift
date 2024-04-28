253716735ed7831a357d6940c523e3be4975f487
Sharaf <sharafa839@gmail.com>
2024-04-28 04:55:37 +0300

    - Encodable+Extension   - create extension for encode data

---
+++ ConcurrencyNetworkLayer/NetworkLayer/Encodable+Extenstion.swift
@@ -0,0 +1,16 @@
+//
+//  Encodable+Extenstion.swift
+//  ConcurrencyNetworkLayer
+//
+//  Created by Sharaf on 4/28/24.
+//
+
+import Foundation
+
+extension Encodable {
+    
+    var dictionary: [String: Any]? {
+        guard let data = (try? JSONEncoder().encode(self)) else { return nil }
+        return (try? JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed)).flatMap{$0 as? [String: Any]}
+    }
+}
